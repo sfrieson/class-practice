c39d6da33bd347621c5e9e38b5461cab0ba93712
Steven Frieson <sfrieson@gmail.com>
2018-07-03 19:10:59 -0400

    Set up scaffolding

--- scripts.js
+++ scripts.js
@@ -1,1 +1,42 @@
+/*
+ItemInventory Class
+properties:
+- _store (map, key: item.name, value: int)
+methods:
+- addItem
+- remove
+- replenish
+*/
 
+/*
+Item class
+properties:
+- name
+- price
+- details
+- pictureUrl
+- quantity
+*/
+
+function renderItems (items, inventory) {
+
+}
+
+/*
+Cart class
+properties:
+- items
+methods:
+- add
+- save
+- load
+- getTotal
+*/
+
+function renderCart (cart) {
+
+}
+
+// Needed for initial render when the page is loaded.
+renderItems();
+renderCart();
